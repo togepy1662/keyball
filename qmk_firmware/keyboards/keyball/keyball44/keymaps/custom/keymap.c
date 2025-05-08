77b131f924ed14ce5b3c79bffebb8c805e4a4076
togepy <bankokuki@gmail.com>
2025-05-08 22:21:04 +0900

    init

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/custom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/custom/keymap.c
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  // keymap for default (VIA)
+  // keymap for default
   [0] = LAYOUT_universal(
     KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
     KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
